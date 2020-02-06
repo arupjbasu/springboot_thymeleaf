3ecde8a6d70784cff2b29d44ac730e4f37f522a4
Arup Basu <abasu1@ra.rockwell.com>
2020-02-06 12:20:20 +0530

    initial Commit

---
+++ src/main/java/com/arup/ThymeL/ThymeLApplication.java
@@ -0,0 +1,13 @@
+package com.arup.ThymeL;
+//   RUN USING URL = http://localhost:8080 AND URL = http://localhost:8080/hello?name=abc
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLApplication.class, args);
+	}
+
+}
